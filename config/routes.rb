82a4477c06b410f41ff0e54ce5a86cc372e4563f
Karl Heitmann <khf1988@gmail.com>
2021-02-27 15:24:22 -0300

    modificadas las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  # get 'posts', to: 'posts#index' # EQUIVALENTE CON LA SIGUIENTE LINEA
+  get 'posts' => 'posts#index'
+  # XXX IMPORTANTE: Recuerde el primer parametro que sigue a get
+  # es lo que aparece en la ruta
   get 'posts/show'
+
+  root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
